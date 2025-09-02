4e0edd3dd020efa7b891ce9364bc8156d3e175f4
akundu0 <akundu@unc.edu>
2025-09-02 00:58:43 -0400

    added basic get, post, and search by id endpoints

--- src/main/java/com/akundu/traveljournal/model/RecordsModel.java
+++ src/main/java/com/akundu/traveljournal/model/RecordsModel.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Entity
-@Table(name = "records_model")
+//@Table(name = "records_model")
 public class RecordsModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
